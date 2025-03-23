8741ddce1bf4b691d9aa1f7098cf3d011acd2c63
onumirea <mireaoana555@gmail.com>
2025-03-23 22:43:44 +0200

    Static_pages_done

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "application#hello"
+  root "static_pages#home"
 end
